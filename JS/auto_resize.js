71f8e9f1cc29d9ee8bac938d7ba1790aebb43214
LilleAndersen <71332089+LilleAndersen@users.noreply.github.com>
2021-01-11 19:21:31 +0100

    added proper resizing of iframe

---
+++ JS/auto_resize.js
@@ -0,0 +1,23 @@
+// Resize iframes when loading
+window.onload = function ()
+{
+    const iframes = document.getElementsByTagName("iframe");
+
+    for (let i = 0; i < iframes.length; i++)
+    {
+        console.log(iframes[i].offsetWidth);
+        iframes[i].style.height = iframes[i].offsetWidth / 16 * 9 + "px";
+    }
+};
+
+// Resize iframes on window resize
+$(window).on('resize',function ()
+{
+    const iframes = document.getElementsByTagName("iframe");
+
+    for (let i = 0; i < iframes.length; i++)
+    {
+        console.log(iframes[i].offsetWidth);
+        iframes[i].style.height = iframes[i].offsetWidth / 16 * 9 + "px";
+    }
+});
