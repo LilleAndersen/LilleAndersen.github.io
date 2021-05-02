4aadc458c8610934cb724f58dfb2355f219d646e
LilleAndersen <71332089+LilleAndersen@users.noreply.github.com>
2021-05-02 21:12:32 +0200

    changed picture names, removed unneccesary code, added variables so certain colors

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -15,9 +15,3 @@ $(document).ready(function(){
         $(".bodyP").removeClass("adamfade");
     });
 });
-
-document.body.onmousemove = function(e) {
-    document.documentElement.style.setProperty('--x', (e.clientX+window.scrollX) + 'px');
-    document.documentElement.style.setProperty('--y', (e.clientY+window.scrollY) + 'px');
-}
-
