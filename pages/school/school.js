9df7c1374f2bb543e5966dedf40ae6e97fe48589
LilleAndersen <71332089+LilleAndersen@users.noreply.github.com>
2021-09-14 10:49:00 +0200

    improve subpage

--- pages/school/school.js
+++ pages/school/school.js
@@ -5,14 +5,22 @@ fetch("school.json")
     {
         for (let i = 0; i < data.school.length; i++)
         {
+            let oddeven = "";
+            console.log(i)
+            if ( i % 2 === 0) {
+                oddeven = "even";
+            } else {
+                oddeven = "odd";
+            }
             document.querySelector("#schoolcontent").innerHTML += `
-                <div id="${data.school[i].title.replace(/\s/g, "")}" class="subpage-content-flex">
+                <div id="${data.school[i].title.replace(/\s/g, "")}" class="subpage-content-flex ${oddeven}">
                 ${data.school[i].media} <br>
-                <p class="date"> 
-                    ${data.school[i].date} 
-                </p>
                 <p class="content"> 
-                    <a class="projectname"> ${data.school[i].title} </a>
+                    <b class="date"> 
+                    ${data.school[i].date}
+                    <br>
+                    </b>
+                    <b class="projecttitle"> ${data.school[i].title} </b>
                     <br> ${data.school[i].text}
                 </p>
                 </div>
