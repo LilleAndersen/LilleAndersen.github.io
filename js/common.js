71b03cbf8c972e691e3ad0fa21f782171e1339db
LilleAndersen <71332089+LilleAndersen@users.noreply.github.com>
2021-09-17 11:54:46 +0200

    remove 700 lines of comments

--- js/common.js
+++ js/common.js
@@ -1,7 +1,6 @@
 $("#header-pre").load("/sections/header.html")
 
 
-
 // window.onresize = function () {
 //     if (window.innerWidth <= 500) {
 //         document.querySelector("#header-pre").innerHTML = "";
@@ -13,43 +12,21 @@ $("#header-pre").load("/sections/header.html")
 //     }
 // }
 
-// const blueHover = '#subpage-header #homebtn:hover, #schoolbtn:hover, #freetimebtn:hover { color: rgb(23,23,82) }';
-// const pinkHover = '#subpage-header #homebtn:hover, #schoolbtn:hover, #freetimebtn:hover { color: rgb(251,99,81) }';
-// const style = document.createElement('style');
 
 // Changes visibility in #backtotop after certain amount of px is scrolled
 window.onscroll = function () {
     if (window.pageYOffset >= 900) {
         document.querySelector("#backtotop").style.opacity="1";
         document.querySelector("#background").style.opacity="0";
-        // document.querySelector("#subpage-header #header").style.boxShadow="0 0 2rem var(--bkg)";
-        // document.querySelector("#subpage-header #header").style.padding=".5vw 0 .5vw 0";
-        // document.querySelector("#subpage-header #header").style.background="rgba(251,99,81,.8)";
-        // document.querySelector("#subpage-header #homebtn:hover, #schoolbtn:hover, #freetimebtn:hover").style.color="rgb(23,23,82)";
-        // if (style.styleSheet) {
-        //     style.styleSheet.cssText = blueHover;
-        // } else {
-        //     style.appendChild(document.createTextNode(blueHover));
-        // }
         // document.getElementById("backtotop").style.display="inline-block";
     }
     else {
         document.querySelector("#backtotop").style.opacity="0";
         document.querySelector("#background").style.opacity="100";
-        // document.querySelector("#subpage-header #header").style.boxShadow="0";
-        // document.querySelector("#subpage-header #header").style.padding="2vw 0 2vw 0";
-        // document.querySelector("#subpage-header #header").style.background="none";
-        // document.querySelector("#subpage-header #homebtn:hover, #schoolbtn:hover, #freetimebtn:hover").style.color="rgb(251,99,81)";
-        // if (style.styleSheet) {
-        //     style.styleSheet.cssText = pinkHover;
-        // } else {
-        //     style.appendChild(document.createTextNode(pinkHover));
-        // }
         // document.getElementById("backtotop").style.display="none";
     }
 }
 
-// document.getElementsByTagName('head')[0].appendChild(style);
 
 // Resize timeout
 //setTimeout(function () {
